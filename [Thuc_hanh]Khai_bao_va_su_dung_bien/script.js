a5e6bda7285abcc0f4fe99bae75331ed58b8e33b
TuLiP-411 <65971089+TuLiP-411@users.noreply.github.com>
2022-07-11 11:59:17 +0700

    Add files via upload

---
+++ [Thuc_hanh]Khai_bao_va_su_dung_bien/script.js
@@ -0,0 +1,30 @@
+// // bai1
+// let i = 10
+// let f = 20.5
+// let b = true
+// let s = 'Hà Nội'
+//
+// document.write('i = ' + i)
+// document.write('<br/>')
+// document.write('f = ' + f)
+// document.write('<br/>')
+// document.write('b = ' + b)
+// document.write('<br/>')
+// document.write('s = ' + s)
+//
+// //bai2
+// let width = 20
+// let height = 10
+// let area = width * height
+// document.write('Area = ' + area)
+
+//bai3
+inputa = prompt("Nhập số a");
+inputb = prompt("Nhập số b");
+
+let a = parseInt(inputa);
+let b = parseInt(inputb);
+
+if (a%b == 0)
+    alert("a là bội số của b")
+else alert("a không là bội số của b")
